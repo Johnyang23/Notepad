b727f5506fd225dde2151f4814067fe0a2fd797f
John Yang <johnyang1996@gmail.com>
2018-04-04 14:54:29 -0400

    Notepad

---
+++ notepad/notepad/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace notepad
+{
+    public partial class Form1 : Form
+    { 
+        int stat = 0;
+    
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox1.BackColor = Color.White;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream ms;
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                if((ms = open.OpenFile()) != null)
+                {
+                    string filename = open.FileName;
+                    string filetext = File.ReadAllText(filename);
+                    richTextBox1.Text = filetext;
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Text Document(*.txt)|*.txt|All Files(*.*)|";
+            if(save.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SaveFile(save.FileName, RichTextBoxStreamType.PlainText);
+                this.Text = save.FileName;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo();
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Redo();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+        }
+
+        private void fontToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            FontDialog font = new FontDialog();
+            font.Font = richTextBox1.SelectionFont;
+            if (font.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = font.Font;
+            }
+        }
+
+        private void fontColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog fcolor = new ColorDialog();
+            if(fcolor.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.ForeColor = fcolor.Color;
+            }
+        }
+
+        private void backgroundColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog bcolor = new ColorDialog();
+
+            if(bcolor.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.BackColor = bcolor.Color;
+            }
+        }
+
+        private void statusBarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (stat % 2 == 0)
+            {
+                statusStrip1.Visible = false;
+            }
+            else
+            {
+                statusStrip1.Visible = true;
+            }
+
+            stat++;
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("George Brown College Notepad Ver 3.2 \nby \nJohn Yang");
+        }
+    }
+}
+
